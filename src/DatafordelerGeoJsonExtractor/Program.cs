364ba4b658f4495cac265e738bb6d2361c66a575
Rune Nielsen <runenielsen@runbox.com>
2022-10-11 10:17:29 +0200

    now retrieves settings for appsettings

--- src/DatafordelerGeoJsonExtractor/Program.cs
+++ src/DatafordelerGeoJsonExtractor/Program.cs
@@ -2,6 +2,7 @@
 using Serilog;
 using Serilog.Events;
 using Serilog.Formatting.Compact;
+using System.Text.Json;
 
 namespace DatafordelerGeoJsonExtractor;
 
@@ -9,22 +10,23 @@ internal static class Program
 {
     public static async Task Main()
     {
-        // TODO this is some BS, should parse arguments from stdin.
-        var setting = new Setting(
-            ftpSetting: new FtpSetting("", "", ""));
+        using var serviceProvider = BuildServiceProvider();
 
-        using var serviceProvider = BuildServiceProvider(setting);
+        var setting = serviceProvider.GetService<Setting>();
+
+        if (setting is null)
+        {
+            throw new InvalidOperationException("Could not get setting.");
+        }
 
         using var cancellationToken = new CancellationTokenSource();
 
-        await Task.CompletedTask.ConfigureAwait(false);
-
         await MatrikelExtract
-            .StartAsync(setting.FtpSetting)
+            .StartAsync(setting.FtpSetting, cancellationToken.Token)
             .ConfigureAwait(false);
     }
 
-    private static ServiceProvider BuildServiceProvider(Setting setting)
+    private static ServiceProvider BuildServiceProvider()
     {
         var logger = new LoggerConfiguration()
             .MinimumLevel.Information()
@@ -33,6 +35,12 @@ internal static class Program
             .Enrich.FromLogContext()
             .WriteTo.Console(new CompactJsonFormatter())
             .CreateLogger();
+
+        var settingsJson = JsonDocument.Parse(File.ReadAllText("appsettings.json"))
+            .RootElement.GetProperty("settings").ToString();
+
+        var setting = JsonSerializer.Deserialize<Setting>(settingsJson) ??
+            throw new ArgumentException("Could not deserialize appsettings into settings.");
 
         return new ServiceCollection()
             .AddLogging(logging =>
--- src/DatafordelerGeoJsonExtractor/Settings.cs
+++ src/DatafordelerGeoJsonExtractor/Settings.cs
@@ -1,11 +1,31 @@
+using System.Text.Json.Serialization;
+
 namespace DatafordelerGeoJsonExtractor;
+
+internal sealed record MatrikelSetting
+{
+    [JsonPropertyName("datasets")]
+    private Dictionary<string, bool> Datasets { get; init; }
+
+    [JsonConstructor]
+    public MatrikelSetting(Dictionary<string, bool> datasets)
+    {
+        Datasets = datasets;
+    }
+}
 
 internal sealed record FtpSetting
 {
-    public string Host { get; }
-    public string Username { get; }
-    public string Password { get; }
+    [JsonPropertyName("host")]
+    public string Host { get; init; }
 
+    [JsonPropertyName("username")]
+    public string Username { get; init; }
+
+    [JsonPropertyName("password")]
+    public string Password { get; init; }
+
+    [JsonConstructor]
     public FtpSetting(string host, string username, string password)
     {
         if (string.IsNullOrWhiteSpace(host))
@@ -37,10 +57,23 @@ internal sealed record FtpSetting
 
 internal sealed record Setting
 {
-    public FtpSetting FtpSetting { get; }
+    [JsonPropertyName("setting")]
+    public FtpSetting FtpSetting { get; init; }
 
-    public Setting(FtpSetting ftpSetting)
+    [JsonPropertyName("outDirPath")]
+    public string OutPath { get; init; }
+
+    [JsonPropertyName("matrikel")]
+    public MatrikelSetting Matrikel { get; init; }
+
+    [JsonConstructor]
+    public Setting(
+        FtpSetting ftpSetting,
+        string outPath,
+        MatrikelSetting matrikel)
     {
         FtpSetting = ftpSetting;
+        OutPath = outPath;
+        Matrikel = matrikel;
     }
 }
